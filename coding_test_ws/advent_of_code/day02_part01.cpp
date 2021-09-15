eb588ee6807dfe74f794645c9b86c2451d201559
Hyunbin <hyunbin.park1026@gmail.com>
2021-09-15 21:54:35 +0000

    Update 'count multiple appearing times' by using 'find_if'

--- coding_test_ws/advent_of_code/day02_part01.cpp
+++ coding_test_ws/advent_of_code/day02_part01.cpp
@@ -2,25 +2,24 @@
 
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <map>
+#include <algorithm>
 
 int main(int argc, char *argv[])
 {
-    std::map<char, int> find_same_char;
+    std::map<char,int> find_same_char;
     std::string line;
     int appear_two_times = 0;
     int appear_three_times = 0;
-    bool found_two = false;
-    bool found_three = false;
-    // int c=0; //* debug
+    int c=0; //* debug
 
     std::ifstream* contents = new std::ifstream("../input/day02_input01.txt");
+                std::cout << "appear_two_times : " << appear_two_times << std::endl; //* debug
 
     //* parse lines
     while(*contents >> line)
     {
-        // std::cout << c++ << ", " << line << std::endl; //* debug
+        std::cout << c++ << ", " << line << std::endl; //* debug
         find_same_char.clear();
 
         //* parse and count each letters
@@ -37,45 +36,20 @@ int main(int argc, char *argv[])
             else
             {
                 it->second++;
-                // std::cout << "Found : " << it->first << ", " << it->second << std::endl; //* debug
+                std::cout << "Found : " << it->first << ", " << it->second << std::endl; //* debug
             }
         }
-
-        found_two = false;
-        found_three = false;
 
         //* count multiple appearing times
-        for (std::map<char,int>::iterator it = find_same_char.begin(); it != find_same_char.end(); it++)
-        {
-            if(it->second == 2)
-            {
-                //* skip if already counted
-                if(found_two)
-                {
-                    continue;
-                }
-                appear_two_times++;
-                found_two = true;
-                // std::cout << "appear_two_times : " << appear_two_times << std::endl; //* debug
-            }
-            else if(it->second == 3)
-            {
-                //* skip if already counted
-                if(found_three)
-                {
-                    continue;
-                }
-                appear_three_times++;
-                found_three = true;
-                // std::cout << "appear_three_times : " << appear_three_times << std::endl; //* debug
-            }
-        }
+        auto it = std::find_if(find_same_char.cbegin(), find_same_char.cend(), [&](const std::pair<char,int>& pair){return (pair.second == 2) ? appear_two_times++ : false;});
+        it = std::find_if(find_same_char.cbegin(), find_same_char.cend(), [&](const std::pair<char,int>& pair){return (pair.second == 3) ? appear_three_times++ : false;});
+        std::cout << "appear_two_times : " << appear_two_times << std::endl; //* debug
     }
 
-    std::cout << "appear_two_times : " << appear_two_times << std::endl;
-    std::cout << "appear_three_times : " << appear_three_times << std::endl;
+    // std::cout << "appear_two_times : " << appear_two_times << std::endl;
+    // std::cout << "appear_three_times : " << appear_three_times << std::endl;
 
-    std::cout << "A checksum : " << appear_two_times * appear_three_times << std::endl;
+    // std::cout << "A checksum : " << appear_two_times * appear_three_times << std::endl;
 
     return 0;
 }
