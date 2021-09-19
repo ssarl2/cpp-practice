29abadf5f5297d8b026662235a4955e6ab4b3f7b
Hyunbin <hyunbin.park1026@gmail.com>
2021-09-19 13:15:24 +0300

    Add detail plans. Other code is not tested

--- coding_test_ws/advent_of_code/day03_part01.cpp
+++ coding_test_ws/advent_of_code/day03_part01.cpp
@@ -2,23 +2,46 @@ https://adventofcode.com/2018/day/3
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+class Claim
+{
+public:
+    Claim(std::string claim);
+    ~Claim();
+    void print_fabric();
+private:
+    void parse_claim(std::string claim);
+    int x_from_left;
+    int y_from_bottom;
+    int width_fabric;
+    int height_fabric;
+};
 
 int main(int argc, char *argv[])
 {
-    std::string lines;
+    std::string line;
     std::ifstream* contents = new std::ifstream("../input/day03_input01.txt");
+    std::vector<Claim> claims;
 
     //* parse lines
     while(*contents >> line)
     {
-        lines.push_back(line);
+        Claim claim{line};
+        claims.push_back(claim);
     }
 
     //* create n claim objects
     //  * details of the object
-    //  * ..
+    //  * a variable of x for the starting point
+    //  * a variable of y for the starting point
+    //  * a variable of the fabric width
+    //  * a variable of the fabric height
+    //  * a function of printing a fabric (when it prints, reverse y value)
     //* compare all each objects
-    //* calculate overlapped part of rectangles and save it for the result
+    //  * a function of comparing two claims whether they overlap
+    //    * check two of width of objects from the starting point whether they overlap and loop as much as height
+    //    * calculate overlapped part of rectangles and save it for the result
 
     return 0;
 }
