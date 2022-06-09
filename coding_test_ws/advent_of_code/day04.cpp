e83c87bb9eeb31a053c3ba51690e01b51064926e
Hyunbin <hyunbin.park1026@gmail.com>
2022-06-10 01:16:13 +0300

    Add plans as pseudocode and an input

--- coding_test_ws/advent_of_code/day04.cpp
+++ coding_test_ws/advent_of_code/day04.cpp
@@ -1,1 +1,19 @@
-TBD
+// https://adventofcode.com/2018/day/4
+
+/* key point values
+ * id : map - key
+ * asleep_total : map - value(array), append
+ * asleep_most : index value - asleep_total
+ * words : wakes up, falls asleep, Guard #? begins shift
+ */
+
+/* functions
+ * result : multiply id by the most minutes asleep
+ * process1 : pick the most minutes id
+ * process2 : make a structure or a class
+ * process3 : count asleep minutes
+ * process4 : store the count in the asleep_total array and the id
+ * process5 : organize the order of the date as ascent
+ * process? : ??
+ */
+// 
