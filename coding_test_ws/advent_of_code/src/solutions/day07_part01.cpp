9b9ddd0774e35df021e7089633184fdde6f7a9d5
HyunbinPark <hyunbin.park1026@gmail.com>
2023-01-13 00:22:18 +0200

    Skeleton of the idea

--- coding_test_ws/advent_of_code/src/solutions/day07_part01.cpp
+++ coding_test_ws/advent_of_code/src/solutions/day07_part01.cpp
@@ -1,6 +1,56 @@
 // https://adventofcode.com/2018/day/7
 
 #include <iostream>
+#include <vector>
+#include <tools.h>
+
+class Node
+{
+public:
+    Node(char node)
+    {
+        node_ = node;
+    }
+    bool hasNext()
+    {
+        if(!next_nodes_.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    void setNextNode(std::vector<Node> next_nodes)
+    {
+        // would this be shallow copy or deep copy?
+        next_nodes_ = next_nodes;
+    }
+private:
+    char node_;
+    std::vector<Node> next_nodes_;
+};
+
+class NodeTree
+{
+public:
+    NodeTree(std::vector<Node> nodes)
+    {
+        nodes_ = nodes;
+        connectNodes();
+    }
+    void solve()
+    {
+
+    }
+private:
+    void connectNodes()
+    {
+
+    }
+    std::vector<Node> nodes_;
+};
 
 int main(int argc, char *argv[])
 {
