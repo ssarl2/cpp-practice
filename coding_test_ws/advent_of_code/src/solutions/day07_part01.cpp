0177de7c0ea38539643b20ed4458638dc04273b4
HyunbinPark <hyunbin.park1026@gmail.com>
2023-01-12 23:53:53 +0200

    Add a solution plan

--- coding_test_ws/advent_of_code/src/solutions/day07_part01.cpp
+++ coding_test_ws/advent_of_code/src/solutions/day07_part01.cpp
@@ -7,5 +7,10 @@ int main(int argc, char *argv[])
     return 0;
 }
 /*
-* 
+* 1. Make a node or a tree with given input
+*    First, find first and last alphabets.
+*    second, attach nodes(alphabets) from the first node(alphabet).
+* 2. Arrange the order with below tips
+*    Current order has all available next alphabets.
+*    Alphavets can be compared as askii code.
 */
