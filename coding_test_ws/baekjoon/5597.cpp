079243cff014fab13281fdb47460cf79464280be
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-04-11 16:43:22 +0300

    Update a solution for 5597

--- coding_test_ws/baekjoon/5597.cpp
+++ coding_test_ws/baekjoon/5597.cpp
@@ -7,26 +7,21 @@ int main()
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    int number_of_students = 30;
-
-    int students[number_of_students];
-    for (int i = 0; i < number_of_students; i++)
-    {
-        students[i] = i + 1;
-    }
+    int number_of_students           = 30;
+    int students[number_of_students] = {0};
 
     int n;
     for (int i = 0; i < number_of_students - 2; i++)
     {
         std::cin >> n;
-        students[n - 1] = 0;
+        students[n - 1] = 1;
     }
 
     for (int i = 0; i < number_of_students; i++)
     {
-        if (students[i] != 0)
+        if (students[i] == 0)
         {
-            std::cout << students[i] << "\n";
+            std::cout << i + 1 << "\n";
         }
     }
 
