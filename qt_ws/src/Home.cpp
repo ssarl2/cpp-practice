412abf1ab9b891a100bfb27310398f7cc4759de1
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-04-12 21:54:33 +0300

    WIP5: Update dynamic menu bar and button size

--- qt_ws/src/Home.cpp
+++ qt_ws/src/Home.cpp
@@ -14,11 +14,7 @@ Home::Home(QWidget* parent) : QWidget(parent)
     QMenu*   test_menu   = createMenu(menu_bar_, "test_menu");
     QAction* test_action = createAction(test_menu, "test_action");
 
-    progress_bar_btn_ = new QPushButton("pb_btn", this);
-    progress_bar_btn_->move(20, 40);
-    progress_bar_btn_->setStyleSheet("background-color: blue;");
-
-    qDebug() << "Home ctor";
+    progress_bar_btn_ = new QPushButton("Progress Bar", this);
 }
 
 Home::~Home()
@@ -30,14 +26,17 @@ void Home::resizeEvent(QResizeEvent* event)
 {
     QWidget::resizeEvent(event);
     int width_btn  = static_cast<int>(static_cast<double>(width()) * 0.2);
-    int height_btn = static_cast<int>(static_cast<double>(height()) * 0.2);
+    int height_btn = static_cast<int>(static_cast<double>(height()) * 0.12);
     int x_btn      = static_cast<int>(static_cast<double>(width()) * 0.07);
     int y_btn      = static_cast<int>(static_cast<double>(height()) * 0.1) + 20;
-    int font_size  = static_cast<int>(static_cast<double>(width_btn) * 0.18);
+    int font_size  = static_cast<int>(static_cast<double>(width_btn) * 0.13);
     std::stringstream ss;
-    ss << "QPushButton {font-size:" << font_size
+    ss << "QPushButton {font-size:" << (font_size > 0 ? font_size : 1)
        << "px; background-color: blue;}";
+
+    menu_bar_->resize(width(), 22);
     progress_bar_btn_->setStyleSheet(ss.str().c_str());
+    progress_bar_btn_->move(x_btn, y_btn);
     progress_bar_btn_->resize(width_btn, height_btn);
 }
 
--- qt_ws/src/ProgressBar.cpp
+++ qt_ws/src/ProgressBar.cpp
@@ -7,14 +7,11 @@
 
 ProgressBar::ProgressBar(QWidget* parent) : QWidget(parent)
 {
-    qDebug() << "Progressbar ctor";
     menu_bar_            = createMenuBar(this);
     QMenu*   test_menu   = createMenu(menu_bar_, "test_menu");
     QAction* test_action = createAction(test_menu, "test_action");
 
-    home_btn_ = new QPushButton("my_button", this);
-    home_btn_->move(20, 40);
-    home_btn_->setStyleSheet("background-color: red;");
+    home_btn_ = new QPushButton("Home", this);
 }
 
 ProgressBar::~ProgressBar()
@@ -31,13 +28,15 @@ void ProgressBar::resizeEvent(QResizeEvent* event)
 {
     QWidget::resizeEvent(event);
     int width_btn  = static_cast<int>(static_cast<double>(width()) * 0.2);
-    int height_btn = static_cast<int>(static_cast<double>(height()) * 0.2);
+    int height_btn = static_cast<int>(static_cast<double>(height()) * 0.12);
     int x_btn      = static_cast<int>(static_cast<double>(width()) * 0.07);
     int y_btn      = static_cast<int>(static_cast<double>(height()) * 0.1) + 20;
     int font_size  = static_cast<int>(static_cast<double>(width_btn) * 0.18);
     std::stringstream ss;
     ss << "QPushButton {font-size:" << font_size
        << "px; background-color: red;}";
+
+    menu_bar_->resize(width(), 22);
     home_btn_->setStyleSheet(ss.str().c_str());
     home_btn_->move(x_btn, y_btn);
     home_btn_->resize(width_btn, height_btn);
