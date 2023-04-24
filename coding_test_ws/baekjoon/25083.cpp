3929943c11c246a0ee9c67dbea91d3776ff5dc4b
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-04-24 10:18:22 +0300

    Add a solution for 25083

---
+++ coding_test_ws/baekjoon/25083.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/25083
+
+#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+
+    std::cout << "         ,r'\"7" << '\n';
+    std::cout << "r`-_   ,'  ,/" << '\n';
+    std::cout << " \\. \". L_r'" << '\n';
+    std::cout << "   `~\\/" << '\n';
+    std::cout << "      |" << '\n';
+    std::cout << "      |" << '\n';
+
+    return 0;
+}
