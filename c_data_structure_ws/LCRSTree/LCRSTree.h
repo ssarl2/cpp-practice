3aba0ae87faa956671cc049d6f291d9e5c41d148
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-04-24 11:37:36 +0300

    Add code of Left Child Right Sibling Tree

---
+++ c_data_structure_ws/LCRSTree/LCRSTree.h
@@ -0,0 +1,24 @@
+#ifndef __LCRSTREE_H__
+#define __LCRSTREE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+
+typedef struct tagLCRSNode
+{
+    struct tagLCRSNode *LeftChild;
+    struct tagLCRSNode *RightSibling;
+
+    ElementType Data;
+} LCRSNode;
+
+LCRSNode *LCRS_CreateNode(ElementType NewData);
+void LCRS_DestroyNode(LCRSNode *Node);
+void LCRS_DestroyTree(LCRSNode *Root);
+
+void LCRS_AddChildNode(LCRSNode *Parent, LCRSNode *Child);
+void LCRS_PrintTree(LCRSNode *Node, int Depth);
+
+#endif // __LCRSTREE_H__
