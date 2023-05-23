3f2b415ac1f1f890624c34720cec69c73cb3266c
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-23 19:33:13 +0300

    Add code of Graph Traversal

---
+++ c_data_structure_and_algorithm_ws/algorithm/GraphTraversal/GraphTraversal.h
@@ -0,0 +1,10 @@
+#ifndef __GRAPHTRAVERSAL_H__
+#define __GRAPHTRAVERSAL_H__
+
+#include "../Graph/Graph.h"
+#include "LinkedQueue.h"
+
+void DFS(Vertex *V);
+void BFS(Vertex *V, LinkedQueue *Queue);
+
+#endif // __GRAPHTRAVERSAL_H__
