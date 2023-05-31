46f71d9f7ce53d56f1b953a6578275b0d3c9ab34
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-05-31 14:02:31 +0300

    Add code of Greedy - MakingChange program

---
+++ c_data_structure_and_algorithm_ws/algorithm/Greedy/MakingChange.h
@@ -0,0 +1,8 @@
+#ifndef __MAKINGCHANGE_H__
+#define __MAKINGCHANGE_H__
+
+void GetChange(int Price, int Pay, int CoinUnits[], int Change[], int Size);
+int CountCoins(int Amount, int CoinUnit);
+void PrintChange(int CoinUnits[], int Change[], int Size);
+
+#endif // __MAKINGCHANGE_H__
