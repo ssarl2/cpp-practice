5f95ab5ce122753f765fca269e18f91c775eb423
Hyunbin Park <hyunbin.park1026@gmail.com>
2023-06-26 23:03:51 +0300

    Add a solution for 24266

---
+++ coding_test_ws/baekjoon/24266.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/24266
+
+#include <iostream>
+
+int main()
+{
+    long long int n;
+    std::cin >> n;
+    std::cout << n * n * n << "\n" << 3 << std::endl;
+    return 0;
+}
